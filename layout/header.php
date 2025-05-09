1575ebe7efbba1c8a20446f2575b607b65dcda75
Chuck <cmrivera0925@gmail.com>
2025-05-09 16:42:26 -0400

    Changed files to php; drafted navbar

---
+++ layout/header.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-yellow border-bottom border-secondary-subtle">
+        <div class="container-lg">
+            <!-- Brand -->
+            <a class="navbar-brand" href="#">
+                <img src="img/kg-coat-of-arms.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
+                KG Board of Directors
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="main-nav">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="dashboard.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="properties.php">Properties</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="board-proposals.php">Board Proposals</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="issues-tracking.php">Issues Tracking</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact-info.php">Contact Info</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
